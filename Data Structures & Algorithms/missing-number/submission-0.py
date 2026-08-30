03434cdf1dcaa8a444ce94f04e4574581e81de80
Sergei Wong <97312683+GumboPasta@users.noreply.github.com>
2026-08-30 17:41:33 -0500

    Update: missing-number - submission-0

--- Data Structures & Algorithms/missing-number/submission-0.py
+++ Data Structures & Algorithms/missing-number/submission-0.py
@@ -2,10 +2,10 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
 
         # Key Data Structure: Math / Bit Manipulation
-
-        res = len(nums)  # start with n itself (accounts for the "extra" expected index)
-
-        for i in range(len(nums)):
+        n = len(nums)
+        res = n  # start with n itself (accounts for the "extra" expected index)
+       
+        for i in range(n):
             # for each index i, add (i - nums[i]) to res
             # this effectively computes: sum(expected indices) - sum(actual values)
             res += (i - nums[i])
